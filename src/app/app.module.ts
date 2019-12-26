cfd1d4857bff73cd75e4cd526d382a0e309f840e
Anzor <48889137+anzorayubov@users.noreply.github.com>
2019-12-26 13:40:39 +0300

    feat: add basic materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSliderModule} from '@angular/material';
+import {MaterialModule} from './material-module';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,7 @@ import {MatSliderModule} from '@angular/material';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
