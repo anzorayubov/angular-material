a9c4794780e8af23086db9541a1cd42736bbd72e
Anzor Ayubov <anzoranzor05@mail.ru>
2020-02-04 17:51:18 +0300

    add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-material';
+  form: FormGroup;
+
+  send() {
+    console.log(this.form.value);
+  }
+  ngOnInit() {
+  this.form = new FormGroup({
+    city: new FormControl('', []),
+    comment: new FormControl('', []),
+    cash: new FormControl('', []),
+    surname: new FormControl('', []),
+    point: new FormControl('', []),
+    name: new FormControl('', []),
+    document: new FormControl('', []),
+    series: new FormControl('', []),
+    docNumber: new FormControl('', []),
+  });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material-module';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import {MaterialModule} from './material-module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
