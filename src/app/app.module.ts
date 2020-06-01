cb66259e31ae5ddc0436485aa99e0ab90f64634f
Anzor Ayubov <anzoranzor05@mail.ru>
2020-06-01 11:36:42 +0300

    add some edits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material-module';
+import { FirtsComponentComponent } from './components/firts-component/firts-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirtsComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/firts-component/firts-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firts-component',
+  templateUrl: './firts-component.component.html',
+  styleUrls: ['./firts-component.component.scss']
+})
+export class FirtsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
