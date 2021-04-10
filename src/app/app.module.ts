d985a80c61adca8e29febc12fa82c86b8d6aafcc
Anzor Ayubov <anzoranzor05@mail.ru>
2021-04-10 12:30:24 +0300

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularMaterialComponent } from './angular-material/angular-material.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AngularMaterialComponent,
     FirstComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
